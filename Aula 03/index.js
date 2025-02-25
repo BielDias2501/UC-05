080fa4476072d3c67b3331031a23ea81d9f1a260
GABRIEL DIAS CUNHA LIMA <a92279958@rn.senac.local>
2025-02-25 16:45:27 -0300

    add: adicionei o delete e o get por id conectado com o banco

--- Aula 03/index.js
+++ Aula 03/index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.get('/produtos', async (requisicao, resposta) => {
     //tratamento de exceções
     try {
-        const consulta = `select * from produto`
-        const produtos = await pool.query(consulta)
-        if (produtos.rows.length === 0) {
+        const consulta = `select * from produto` 
+        const produtos = await pool.query(consulta) //FAZENDO A CONSULTA 
+        if (produtos.rows.length === 0) { //BUSCAR LINHAS NO BANCO DE DADOS\\
             return resposta.status(200).json({ mensagem: "Banco de Dados vazio" })
         }
         resposta.status(200).json(produtos.rows);
@@ -31,10 +31,10 @@ app.post('/produtos', async (requisicao, resposta) => {
         if (!nome || !preco || !quantidade) {
             return resposta.status(200).json({ mensagem: "Todos os dados devem ser preenchidos" })
         }
-        const novoProduto = [nome, preco, quantidade];
+        const dados = [nome, preco, quantidade];
         const consulta = `insert into produto(nome,preco,quantidade) 
                     values ($1, $2, $3) returning *`
-        await pool.query(consulta, novoProduto)
+        await pool.query(consulta, dados)
 
         resposta.status(201).json({ mensagem: "Produto criado com sucesso" });
     } catch (error) {
@@ -51,15 +51,15 @@ app.put('/produtos/:id', async (requisicao, resposta) => {
         if (!id) {
             return resposta.status(404).json({ mensagem: "informe um paramentro" })
         }
-        const parametro = [id]
-        const consulta = `select * from produto where id = $1`
-        const resultado = await pool.query(consulta, parametro)
-        if (!resultado.rows.length === 0) {
+        const dados1 = [id]
+        const consulta1 = `select * from produto where id = $1`
+        const resultado1 = await pool.query(consulta1, dados1)
+        if (!resultado1.rows.length === 0) {
             return resposta.status(404).json({ mensagem: "Produto não encontrado" })
         }
-        const dados = [id, novoNome, novoPreco, novaQuantidade]
-        const consulta2 = `update produto set nome = $2 preco = $3, quantidade = $4 where id = $1 returning *`
-        await pool.query(consulta2,dados)
+        const dados2 = [id, novoNome, novoPreco, novaQuantidade]
+        const consulta2 = `update produto set nome = $2, preco = $3, quantidade = $4 where id = $1 returning *`
+        await pool.query(consulta2, dados2)
         resposta.status(200).json({ mensagem: "Produto atualizado com sucesso" })
     } catch (error) {
         resposta.status(500).json({ mensagem: "Erro ao editar produtos", erro: error.message })
@@ -70,12 +70,15 @@ app.put('/produtos/:id', async (requisicao, resposta) => {
 app.delete("/produtos/:id", async (requisicao, resposta) => {
     try {
         const id = requisicao.params.id
-        const parametro = [id]
-        const consulta1= `select * from produto where id = $1`
-        const resultado1 = await pool.query(consulta1,parametro)
+        const dados1 = [id]
+        const consulta1 = `select * from produto where id = $1`
+        const resultado1 = await pool.query(consulta1, dados1)
         if (resultado1.rows.length === 0) {
             return resposta.status(404).json({ mensagem: "Produto não encontrado" })
         }
+        const dados2 = [id]
+        const consulta2 = `delete from produto where id = $1`
+        await pool.query(consulta2, dados2)
         resposta.status(200).json({ mensagem: "Produto deletado com sucesos" })
     } catch (error) {
         resposta.status(500).json({ mensagem: "Erro ao excluir produtos", erro: error.message })
@@ -85,14 +88,16 @@ app.delete("/produtos/:id", async (requisicao, resposta) => {
 
 //GET PELO ID
 
-app.get('/produtos/:id', (requisicao, resposta) => {
+app.get('/produtos/:id', async (requisicao, resposta) => {
     try {
         const id = requisicao.params.id
-        const produto = bancoDados.find(elemento => elemento.id === id)
-        if (!produto) {
+        const dados1 = [id]
+        const consulta1 = `select * from produto where id = $1`
+        const resultado1 = await pool.query(consulta1, dados1)
+        if (!resultado1.rows.length === 0) {
             return resposta.status(404).json({ mensagem: "Produto não encontrado" })
         }
-        resposta.status(200).json(produto)
+        resposta.status(200).json(resultado1.rows[0])
     } catch (error) {
         resposta.status(500).json({ mensagem: "Erro ao buscar o produto", erro: error.message })
     }
@@ -104,9 +109,10 @@ app.listen(port, () => {
 });
 
 //DELETAR TUDO
-app.delete('/produtos', (requisicao, resposta) => {
+app.delete('/produtos', async (requisicao, resposta) => {
     try {
-        bancoDados.length = 0
+        const consulta = `delete from produto`
+        await pool.query(consulta)
         resposta.status(200).json({ mensagem: "Todos os dados excluídos com sucesso" })
     } catch (error) {
         resposta.status(500).json({ mensagem: "Erro ao deletar todos os produtos", erro: error.message })
